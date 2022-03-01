ff47b2d25985da4f0724671f984b0ea5604ddfed
Velpre <veljko_kv@hotmail.com>
2022-03-01 11:58:07 +0100

    finish with get req

--- client/index.jsx
+++ client/index.jsx
@@ -1,6 +1,71 @@
 import * as ReactDOM from "react-dom";
 import * as React from "react";
+import {Link, Routes, Route, BrowserRouter} from "react-router-dom"
+import {useEffect, useState} from "react";
+
+function FrontPage(){
+    return<div>
+        <h1>Start Quiz</h1>
+        <ul>
+            <li><Link to={"/question"}>Choose question</Link></li>
+        </ul>
+    </div>
+}
+
+
+function RandonQuesiton() {
+
+    const [data, setData] = useState();
+    const [error, setError] = useState();
+    const[loading, setLoading] = useState(true)
+
+    useEffect(async ()=>{
+        try{
+            const res = await fetch("/api/question")
+            setData(await res.json())
+        }catch (e){
+            setError(e);
+        }finally {
+            setLoading(false);
+        }
+    }, [])
+
+    if (loading){
+        return "Loading..."
+    }
+
+    if (error){
+        return {error}
+    }
+
+    return <div>
+            <h1>{data.question}</h1>
+            <p>ID: {data.id} ({data.category})</p>
+
+            <ul>
+                {
+                  Object.keys(data.answers)
+                        .filter(a => data.answers[a] != null)
+                        .map((a, i) => {
+                            return <ul key={i}>
+                                <button>{data.answers[a]}</button>
+                            </ul>
+                        })
+                }
+            </ul>
+        </div>
+}
 
 
 
-ReactDOM.render("<h1>test1</h1>", document.getElementById("root"))
+
+function App (){
+    return <BrowserRouter>
+        <Routes>
+            <Route path={"/"} element={<FrontPage/>}></Route>
+            <Route path={"/question"} element={<RandonQuesiton/>}></Route>
+        </Routes>
+    </BrowserRouter>
+}
+
+ReactDOM.render(<App/>, document.getElementById("root"))
--- server/server.js
+++ server/server.js
@@ -1,10 +1,28 @@
 import express from "express"
+import path from "path";
+import {randomQuestion, isCorrectAnswer } from "./questions.js"
 const app = express();
 
 
-app.get("/", (req,res)=>{
-    res.send("test")
+app.get("/api", (req,res)=>{
+    res.send("første")
 })
+
+app.get("/api/question", (req,res)=>{
+    const {id, category, question, answers} = randomQuestion();
+    res.json({question, answers, id, category,})
+})
+
+
+//Håndterer filer
+app.use(express.static("../client/dist"));
+app.use((req, res, next) => {
+    if (req.method === "GET" && !req.path.startsWith("/api")) {
+        res.sendFile(path.resolve("../client/dist/index.html"));
+    } else {
+        next();
+    }
+});
 
 const server = app.listen(process.env.PORT || 3001,()=>{
     console.log(`server running on: http://localhost:${server.address().port}`);
