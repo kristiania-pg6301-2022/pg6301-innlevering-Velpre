4b8c1a39dda668b3c1fcdca215a288d2e68f1e0e
SigmundGjengedal <sigmund.gjengedal@gmail.com>
2022-03-01 18:09:43 +0100

    added coveralls, jest, GH actions

---
+++ client/Quiz.jsx
@@ -0,0 +1,75 @@
+import * as React from "react";
+import { useState } from "react";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { fetchJSON, postJSON } from "./http.js";
+import { useLoading } from "./useLoading";
+
+export function FrontPage() {
+    return (
+        <div>
+            <h1>Start Quiz</h1>
+            <ul>
+                <li>
+                    <Link to={"/question"}>Choose question</Link>
+                </li>
+            </ul>
+        </div>
+    );
+}
+
+function RandomQuestion() {
+    const { data, error, loading, reload } = useLoading(
+        async () => await fetchJSON("api/question")
+    );
+    const [answeredQuestion, setAnsweredQuestion] = useState();
+
+    async function handleAnswer(answer) {
+        const { id } = data;
+        setAnsweredQuestion(await postJSON("/api/question", answer, id));
+        await reload();
+    }
+
+    if (loading) {
+        return "Loading...";
+    }
+
+    if (error) {
+        return { error };
+    }
+    const answers = Object.keys(data.answers).filter(
+        (a) => data.answers[a] != null
+    );
+    return (
+        <div>
+            <h1>{data.question}</h1>
+            <p>
+                ID: {data.id} ({data.category})
+            </p>
+            <ul>
+                {answers.map((a, i) => {
+                    return (
+                        <ul key={i}>
+                            <button onClick={() => handleAnswer(a)}>{data.answers[a]}</button>
+                        </ul>
+                    );
+                })}
+            </ul>
+            <p>
+                {answeredQuestion
+                    ? "svar på forrige spørsmål:" + answeredQuestion.result.toString()
+                    : null}
+            </p>
+        </div>
+    );
+}
+
+export function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path={"/"} element={<FrontPage />}></Route>
+                <Route path={"/question"} element={<RandomQuestion />}></Route>
+            </Routes>
+        </BrowserRouter>
+    );
+}
