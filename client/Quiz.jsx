2e0ffda77e5886c8afe148e130895a64e7a71757
Velpre <veljko_kv@hotmail.com>
2022-03-02 17:08:24 +0100

    added GA

--- client/Quiz.jsx
+++ client/Quiz.jsx
@@ -5,71 +5,71 @@ import { fetchJSON, postJSON } from "./http.js";
 import { useLoading } from "./useLoading";
 
 export function FrontPage() {
-    return (
-        <div>
-            <h1>Start Quiz</h1>
-            <ul>
-                <li>
-                    <Link to={"/question"}>Choose question</Link>
-                </li>
-            </ul>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Start Quiz</h1>
+      <ul>
+        <li>
+          <Link to={"/question"}>Choose question</Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 function RandomQuestion() {
-    const { data, error, loading, reload } = useLoading(
-        async () => await fetchJSON("api/question")
-    );
-    const [answeredQuestion, setAnsweredQuestion] = useState();
+  const { data, error, loading, reload } = useLoading(
+    async () => await fetchJSON("api/question")
+  );
+  const [answeredQuestion, setAnsweredQuestion] = useState();
 
-    async function handleAnswer(answer) {
-        const { id } = data;
-        setAnsweredQuestion(await postJSON("/api/question", answer, id));
-        await reload();
-    }
+  async function handleAnswer(answer) {
+    const { id } = data;
+    setAnsweredQuestion(await postJSON("/api/question", answer, id));
+    await reload();
+  }
 
-    if (loading) {
-        return "Loading...";
-    }
+  if (loading) {
+    return "Loading...";
+  }
 
-    if (error) {
-        return { error };
-    }
-    const answers = Object.keys(data.answers).filter(
-        (a) => data.answers[a] != null
-    );
-    return (
-        <div>
-            <h1>{data.question}</h1>
-            <p>
-                ID: {data.id} ({data.category})
-            </p>
-            <ul>
-                {answers.map((a, i) => {
-                    return (
-                        <ul key={i}>
-                            <button onClick={() => handleAnswer(a)}>{data.answers[a]}</button>
-                        </ul>
-                    );
-                })}
+  if (error) {
+    return { error };
+  }
+  const answers = Object.keys(data.answers).filter(
+    (a) => data.answers[a] != null
+  );
+  return (
+    <div>
+      <h1>{data.question}</h1>
+      <p>
+        ID: {data.id} ({data.category})
+      </p>
+      <ul>
+        {answers.map((a, i) => {
+          return (
+            <ul key={i}>
+              <button onClick={() => handleAnswer(a)}>{data.answers[a]}</button>
             </ul>
-            <p>
-                {answeredQuestion
-                    ? "svar på forrige spørsmål:" + answeredQuestion.result.toString()
-                    : null}
-            </p>
-        </div>
-    );
+          );
+        })}
+      </ul>
+      <p>
+        {answeredQuestion
+          ? "svar på forrige spørsmål:" + answeredQuestion.result.toString()
+          : null}
+      </p>
+    </div>
+  );
 }
 
 export function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path={"/"} element={<FrontPage />}></Route>
-                <Route path={"/question"} element={<RandomQuestion />}></Route>
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={"/"} element={<FrontPage />}></Route>
+        <Route path={"/question"} element={<RandomQuestion />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
--- client/__test__/quiz.test.jsx
+++ client/__test__/quiz.test.jsx
@@ -1,22 +1,21 @@
 import ReactDOM from "react-dom";
 import React from "react";
-import {FrontPage} from "../Quiz.jsx"
-import { MemoryRouter} from "react-router-dom";
+import { FrontPage } from "../Quiz.jsx";
+import { MemoryRouter } from "react-router-dom";
 
-describe("quiz app", ()=>{
-    it("shows front page", ()=>{
-        const element = document.createElement("div");
-        ReactDOM.render(
-            <MemoryRouter>
-                {" "}
-                <FrontPage />
-            </MemoryRouter>,
-            element
-        );
-        // tester h1
-        expect(element.querySelector("h1").innerHTML).toEqual("Start Quiz");
-        // tester snapshots av diven
-        expect(element.innerHTML).toMatchSnapshot();
-    })
-})
-
+describe("quiz app", () => {
+  it("shows front page", () => {
+    const element = document.createElement("div");
+    ReactDOM.render(
+      <MemoryRouter>
+        {" "}
+        <FrontPage />
+      </MemoryRouter>,
+      element
+    );
+    // tester h1
+    expect(element.querySelector("h1").innerHTML).toEqual("Start Quiz");
+    // tester snapshots av diven
+    expect(element.innerHTML).toMatchSnapshot();
+  });
+});
